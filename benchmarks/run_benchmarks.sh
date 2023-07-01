30b0986e4f892070c7908106a0707fd85951c638
Tobias Theuer <tobias.theuer@gmx.de>
2023-07-01 18:57:08 +0200

    fix run_benchmarks.sh to not abort when there is no old results json file

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -139,13 +139,13 @@ runBenchmark() {
 
 export CLICOLOR_FORCE=1
 
+[ -f "$outputFile" ] && mv "$outputFile" "${outputFile}Old"
+
 cd "${SCRIPTPATH}"
 (mkdir -p ../build/Release/ &&
 cd ../build/Release/ &&
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../../ &&
 cmake --build .) &&
-
-[ -f "$outputFile" ] && mv "$outputFile" "${outputFile}Old" &&
 
 if [[ $quick == 1 ]]; then
     runBenchmark --benchmark_out="$outputFile" --benchmark_out_format=json --benchmark_counters_tabular=true "$@"
